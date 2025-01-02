4983e3c0ad26b39fb9de06c055e39db1070a602e
Вячеслав <@>
2025-01-02 13:40:51 +0200

    new day, new print

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('https://github.com/sezitekipi/git_project1')
+    print('Again')
+    print('UFO came and added this line')
 
 
 if __name__ == '__main__':
